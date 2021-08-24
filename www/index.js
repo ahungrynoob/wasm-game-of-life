7b554cb0a7e4a3dc76e8ef374d486864643ffd0e
dingxuda <dingxuda@bytedance.com>
2021-08-24 21:58:46 +0800

    chore: time profiling

--- www/index.js
+++ www/index.js
@@ -17,7 +17,53 @@ const canvas = document.getElementById("game-of-life-canvas");
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 
+let animationId = null;
+
+
 const ctx = canvas.getContext('2d');
+
+const fps = new class {
+    constructor() {
+      this.fps = document.getElementById("fps");
+      this.frames = [];
+      this.lastFrameTimeStamp = performance.now();
+    }
+  
+    render() {
+      // Convert the delta time since the last frame render into a measure
+      // of frames per second.
+      const now = performance.now();
+      const delta = now - this.lastFrameTimeStamp;
+      this.lastFrameTimeStamp = now;
+      const fps = 1 / delta * 1000;
+  
+      // Save only the latest 100 timings.
+      this.frames.push(fps);
+      if (this.frames.length > 100) {
+        this.frames.shift();
+      }
+  
+      // Find the max, min, and mean of our 100 latest timings.
+      let min = Infinity;
+      let max = -Infinity;
+      let sum = 0;
+      for (let i = 0; i < this.frames.length; i++) {
+        sum += this.frames[i];
+        min = Math.min(this.frames[i], min);
+        max = Math.max(this.frames[i], max);
+      }
+      let mean = sum / this.frames.length;
+  
+      // Render the statistics.
+      this.fps.textContent = `
+  Frames per Second:
+           latest = ${Math.round(fps)}
+  avg of last 100 = ${Math.round(mean)}
+  min of last 100 = ${Math.round(min)}
+  max of last 100 = ${Math.round(max)}
+  `.trim();
+    }
+  };
 
 const drawGrid = () => {
     ctx.beginPath();
@@ -38,13 +84,21 @@ const drawGrid = () => {
     ctx.stroke();
 };
 
+const isPaused = () => {
+    return animationId === null;
+};
+  
+
 const renderLoop = () => {
-  universe.tick();
+    fps.render();
+    for (let i = 0; i < 9; i++) {
+        universe.tick();
+    }
 
   drawGrid();
   drawCells();
 
-  requestAnimationFrame(renderLoop);
+  animationId = requestAnimationFrame(renderLoop);
 };
 
 const getIndex = (row, column) => {
@@ -63,14 +117,31 @@ const drawCells = () => {
     const cells = new Uint8Array(memory.buffer, cellsPtr, width * height / 8);
 
     ctx.beginPath();
+    ctx.fillStyle = ALIVE_COLOR;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            if(!bitIsSet(idx, cells)) {
+                continue;
+            }
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    ctx.fillStyle = DEAD_COLOR;
 
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
             const idx = getIndex(row, col);
-
-            ctx.fillStyle = bitIsSet(idx, cells)
-                ? ALIVE_COLOR
-                : DEAD_COLOR;
+            if(bitIsSet(idx, cells)) {
+                continue;
+            }
 
             ctx.fillRect(
                 col * (CELL_SIZE + 1) + 1,
@@ -86,4 +157,45 @@ const drawCells = () => {
 
 drawGrid();
 drawCells();
-requestAnimationFrame(renderLoop);
+
+
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+  playPauseButton.textContent = "⏸";
+  renderLoop();
+};
+
+play();
+
+const pause = () => {
+  playPauseButton.textContent = "▶";
+  cancelAnimationFrame(animationId);
+  animationId = null;
+};
+
+playPauseButton.addEventListener("click", event => {
+  if (isPaused()) {
+    play();
+  } else {
+    pause();
+  }
+});
+
+canvas.addEventListener("click", event => {
+    const boundingRect = canvas.getBoundingClientRect();
+  
+    const scaleX = canvas.width / boundingRect.width;
+    const scaleY = canvas.height / boundingRect.height;
+  
+    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+  
+    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+    const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+  
+    universe.toggle_cell(row, col);
+  
+    drawGrid();
+    drawCells();
+  });
